b603a2b394d50478268da24999dedd71fe220755
mikhail-lazarau <m.lazarau@godeltech.com>
2021-11-13 22:57:14 +0300

    instantiate new project: cocoapods, localisation, splash screen, AppIcon

---
+++ lostfilm/lostfilm/Controllers/TabBarRootController.swift
@@ -0,0 +1,8 @@
+import UIKit
+
+class TabBarRootController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ lostfilm/lostfilm/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  lostfilm
+//
+//  Created by Mikhail Lazarau on 13.11.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
