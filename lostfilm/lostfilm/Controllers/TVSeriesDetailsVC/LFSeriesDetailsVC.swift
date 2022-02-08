0d29273bfbc9d2129b97d0210234dc137b032be1
mikhail-lazarau <m.lazarau@godeltech.com>
2022-02-09 02:39:48 +0300

    intermediate commit

--- lostfilm/lostfilm/Controllers/TVSeriesDetailsVC/LFSeriesDetailsVC.swift
+++ lostfilm/lostfilm/Controllers/TVSeriesDetailsVC/LFSeriesDetailsVC.swift
@@ -6,7 +6,7 @@ class LFSeriesDetailsVC: UIViewController, CarbonTabSwipeNavigationDelegate {
 
     init(model: LFSeriesModel) {
         viewModel = SeriesVM(dataProvider: TVSeriesOverviewDC(model: model))
-        controllers = [TVSeriesOverviewTVC(style: .grouped, viewModel: viewModel)]
+        controllers = [TVSeriesOverviewTVC(style: .plain, viewModel: viewModel)]
         super.init(nibName: nil, bundle: nil)
     }
 
@@ -18,11 +18,29 @@ class LFSeriesDetailsVC: UIViewController, CarbonTabSwipeNavigationDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        let items = ["Test Title"]
+        let items = ["Обзор сериала"]
         let carbonTabSwipeNavigation = CarbonTabSwipeNavigation(items: items, delegate: self)
         carbonTabSwipeNavigation.insert(intoRootViewController: self)
+        carbonTabSwipeNavigation.toolbar.isTranslucent = false
+        
+        self.navigationItem.title = viewModel.dataProvider?.model.nameRu
+        self.navigationItem.largeTitleDisplayMode = .never
+        carbonTabSwipeNavigation.navigationController?.navigationBar.backgroundColor = .white
+//        carbonTabSwipeNavigation.setNormalColor(.white)
+//        carbonTabSwipeNavigation.carbonSegmentedControl?.backgroundColor = .white
+//        carbonTabSwipeNavigation.toolbar.clipsToBounds = true
     }
 
+//    override func viewWillAppear(_ animated: Bool) {
+//        self.navigationController?.navigationBar.shadowImage = UIImage()
+//        super.viewWillAppear(animated)
+//    }
+//
+//    override func viewWillDisappear(_ animated: Bool) {
+//        self.navigationController?.navigationBar.shadowImage = nil
+//        super.viewWillDisappear(animated)
+//    }
+    
     func carbonTabSwipeNavigation(_ carbonTabSwipeNavigation: CarbonTabSwipeNavigation, viewControllerAt index: UInt) -> UIViewController {
         return controllers[Int(index)]
     }
