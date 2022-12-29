07127e1f7f53a309632f0bb14c21c06635993369
mikhail-lazarau <m.lazarau@godeltech.com>
2022-12-29 18:02:59 +0100

    intermediate commit: enum ToastPosition (only YAxis customizable)

--- lostfilm/lostfilm/Views/CustomElements/AlertPresentingController.swift
+++ lostfilm/lostfilm/Views/CustomElements/AlertPresentingController.swift
@@ -7,8 +7,38 @@
 
 import UIKit
 
+public enum ToastConstraint {
+    case xAxisConstraint, yAxisConstraint
+}
+
+public enum ToastPosition {
+    case top, center, bottom
+
+    fileprivate func setupPosition(toast: UIButton, superview: UIView) -> [ToastConstraint: NSLayoutConstraint] {
+        let xAxisConstraint = toast.centerXAnchor.constraint(equalTo: superview.centerXAnchor)
+        let yAxisConstraint: NSLayoutConstraint
+
+        switch self {
+        case .top:
+            yAxisConstraint = toast.topAnchor.constraint(equalTo: superview.topAnchor, constant: -(superview.safeAreaInsets.top + toast.bounds.height))
+        case .center:
+            yAxisConstraint = toast.centerYAnchor.constraint(equalTo: superview.centerYAnchor, constant: 0)
+        case .bottom:
+            yAxisConstraint = toast.bottomAnchor.constraint(equalTo: superview.bottomAnchor, constant: (superview.safeAreaInsets.bottom + toast.bounds.height))
+        }
+
+        NSLayoutConstraint.activate([
+            xAxisConstraint, yAxisConstraint
+        ])
+
+        return [.xAxisConstraint: xAxisConstraint, .yAxisConstraint: yAxisConstraint]
+    }
+}
+
 class AlertPresentingController: UIViewController {
-    let alertButton: UIButton
+    let toast: UIButton
+    var toastPosition: ToastPosition
+    var toastConstraints: [ToastConstraint: NSLayoutConstraint]?
     weak var windowDelegate: AlertWindowProtocol?
     var topConstraint: NSLayoutConstraint!
     var centerXConstraint: NSLayoutConstraint!
@@ -17,8 +47,9 @@ class AlertPresentingController: UIViewController {
     private let screenWidth: CGFloat = UIScreen.main.bounds.width
     private var dismissalTimer: Timer?
 
-    init(alertButton: UIButton) {
-        self.alertButton = alertButton
+    init(alertButton: UIButton, position: ToastPosition) {
+        self.toast = alertButton
+        toastPosition = position
         super.init(nibName: nil, bundle: nil)
     }
 
@@ -32,17 +63,35 @@ class AlertPresentingController: UIViewController {
         // Do any additional setup after loading the view.
         view.backgroundColor = .clear
         view.isUserInteractionEnabled = false
-        view.addSubview(alertButton)
+        view.addSubview(toast)
 
-        alertButton.translatesAutoresizingMaskIntoConstraints = false
-        alertButton.isUserInteractionEnabled = true
-        alertButton.addTarget(self, action: #selector(DidTapToast), for: .touchUpInside)
+        toast.translatesAutoresizingMaskIntoConstraints = false
+        toast.isUserInteractionEnabled = true
+        toast.addTarget(self, action: #selector(DidTapToast), for: .touchUpInside)
 
-        topConstraint = alertButton.topAnchor.constraint(equalTo: view.topAnchor, constant: -screenHeight / 10)
-        centerXConstraint = alertButton.centerXAnchor.constraint(equalTo: view.centerXAnchor)
-        NSLayoutConstraint.activate([
-            topConstraint, centerXConstraint
-        ])
+        toastConstraints = toastPosition.setupPosition(toast: toast, superview: view)
+
+//        topConstraint = toast.topAnchor.constraint(equalTo: view.topAnchor, constant: -screenHeight / 10)
+//        centerXConstraint = toast.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+//        NSLayoutConstraint.activate([
+//            topConstraint, centerXConstraint
+//        ])
+    }
+
+    func preRenderingPosition(toast: UIButton, superview: UIView) {
+//        topConstraint = alertButton.topAnchor.constraint(equalTo: view.topAnchor, constant: -screenHeight / 10)
+//        centerXConstraint = alertButton.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+//        NSLayoutConstraint.activate([
+//            topConstraint, centerXConstraint
+//        ])
+    }
+
+    func appearingAnimation() {
+
+    }
+
+    func dismissingAnimation() {
+
     }
 
     override func viewDidAppear(_ animated: Bool) {
@@ -80,7 +129,7 @@ class AlertPresentingController: UIViewController {
                 return
             }
             self.centerXConstraint.isActive = false
-            self.alertButton.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: self.screenWidth + self.alertButton.bounds.width).isActive = true
+            self.toast.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: self.screenWidth + self.toast.bounds.width).isActive = true
             self.view.layoutIfNeeded()
         }, completion: { _ in
             self.windowDelegate?.dismissWindow()
