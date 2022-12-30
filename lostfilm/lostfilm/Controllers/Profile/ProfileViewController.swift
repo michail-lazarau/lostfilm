3ccc19936693fb320356be5b2a244ab0193c1d8d
Uladzislau Yanouski <u.yanosuki@gmail.com>
2022-12-30 11:45:29 +0100

    Deleted unused code

--- lostfilm/lostfilm/Controllers/Profile/ProfileViewController.swift
+++ lostfilm/lostfilm/Controllers/Profile/ProfileViewController.swift
@@ -209,40 +209,6 @@ private extension ProfileViewController {
     }
 
     // MARK: Keyboard
-    func registerKeyboardNotification() {
-        NotificationCenter.default.addObserver(self,
-                                               selector: #selector(keyboardWillShow),
-                                               name: UIResponder.keyboardWillShowNotification,
-                                               object: nil)
-
-        NotificationCenter.default.addObserver(self,
-                                               selector: #selector(keyboardWillHide),
-                                               name: UIResponder.keyboardWillHideNotification,
-                                               object: nil)
-
-    }
-
-    func removeKeyboardNotification() {
-        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
-        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
-    }
-
-    @objc func keyboardWillShow(notification: NSNotification) {
-        guard let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue else { return }
-
-        let contentInsert = UIEdgeInsets(top: 0.0, left: 0.0, bottom: keyboardSize.height, right: 0.0)
-
-        scrollView.contentInset = contentInsert
-        scrollView.scrollIndicatorInsets = contentInsert
-    }
-
-    @objc func keyboardWillHide(notification: NSNotification) {
-        let contentInsets = UIEdgeInsets(top: 0.0, left: 0.0, bottom: 0.0, right: 0.0)
-
-        scrollView.contentInset = contentInsets
-        scrollView.scrollIndicatorInsets = contentInsets
-    }
-
     @objc func hideKeyboard() {
         view.endEditing(true)
     }
